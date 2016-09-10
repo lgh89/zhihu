75390b4e79e4d7a5c4947cb361d6b972f7d9c585
lgh <test@163.com>
2016-09-10 17:30:02 +0800

    初始化项目

---
+++ zhihu/zhihu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  zhihu
+//
+//  Created by lgh on 16/9/10.
+//  Copyright © 2016年 lgh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ zhihu/zhihu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  zhihu
+//
+//  Created by lgh on 16/9/10.
+//  Copyright © 2016年 lgh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
